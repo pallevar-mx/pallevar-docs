46f57686876ba57a761fd55aae108cf93f200fe0
Vega <obed.vega@teradata.com>
2024-04-09 12:07:21 -0700

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,23 +5,25 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  const organizationName = 'obedvega'; // Usually your GitHub org/user name.
+  const projectName = 'td-docusaurus'; // Usually your repo name.
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
+//  organizationName,
+//  projectName,
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  //url: 'https://your-docusaurus-site.example.com',
+  url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: `/${projectName}/`,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -31,7 +33,7 @@ const config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en','es','ja'],
+    locales: ['en', 'fr', 'ja'],
     localeConfigs:{
       en:{
         htmlLang:'en-GB'
