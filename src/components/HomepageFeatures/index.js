916794c4211990b2cd27a1bb8c2579841bade91d
Vega <obed.vega@teradata.com>
2024-04-09 19:11:17 -0700

    test

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,62 +1,46 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Translate from '@docusaurus/Translate';
 
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          <div className='col col--4'>
+            <div className="text--center">
+               
+            </div>
+            <div className="text--center padding-horiz--md">
+              <Heading as="h3"><Translate id="theme.subtitle-1">Easy to Use</Translate></Heading>
+              <p>
+              Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.</p>
+            </div>
+          </div>
+          <div className='col col--4'>
+            <div className="text--center">
+               
+            </div>
+            <div className="text--center padding-horiz--md">
+              <Heading as="h3"><Translate id="theme.subtitle-2">Focus on What Matters</Translate></Heading>
+              <p>
+              Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.</p>
+            </div>
+          </div>
+          <div className='col col--4'>
+            <div className="text--center">
+               
+            </div>
+            <div className="text--center padding-horiz--md">
+            <Heading as="h3"><Translate id="theme.subtitle-3">Powered by React</Translate></Heading>
+              <p>Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
