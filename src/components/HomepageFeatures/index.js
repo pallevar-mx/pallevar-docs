9d610ae8a073c8c155f4b6221b1a56706ce127b1
Vega <obed.vega@teradata.com>
2024-04-10 07:44:30 -0700

    test

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -16,8 +16,10 @@ export default function HomepageFeatures() {
             <div className="text--center padding-horiz--md">
               <Heading as="h3"><Translate id="theme.subtitle-1">Easy to Use</Translate></Heading>
               <p>
-              Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.</p>
+                <Translate id="theme.subhead-1">
+                Docusaurus was designed from the ground up to be easily installed and used to get your website up and running quickly.
+                </Translate>
+              </p>
             </div>
           </div>
           <div className='col col--4'>
@@ -27,8 +29,10 @@ export default function HomepageFeatures() {
             <div className="text--center padding-horiz--md">
               <Heading as="h3"><Translate id="theme.subtitle-2">Focus on What Matters</Translate></Heading>
               <p>
-              Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.</p>
+                <Translate id='theme.subhead-3'>
+                Docusaurus lets you focus on your docs, and we ll do the chores. Go ahead and move your docs into the docs directory.
+                </Translate>
+              </p>
             </div>
           </div>
           <div className='col col--4'>
@@ -37,8 +41,11 @@ export default function HomepageFeatures() {
             </div>
             <div className="text--center padding-horiz--md">
             <Heading as="h3"><Translate id="theme.subtitle-3">Powered by React</Translate></Heading>
-              <p>Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.</p>
+              <p>
+              <Translate id="theme.subhead-3">
+                Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same header and footer.
+                </Translate>
+              </p>
             </div>
           </div>
         </div>
