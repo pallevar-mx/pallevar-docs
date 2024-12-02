b7dc66e7daa04fefd04fcbf77244852035f332a2
Vega <obed.vega@teradata.com>
2024-12-02 11:25:29 -0800

    new org name-1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'pallevar-me', // Usually your GitHub org/user name.
-  projectName: 'pallevar-jobs', // Usually your repo name.
+  projectName: 'pallevar-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
